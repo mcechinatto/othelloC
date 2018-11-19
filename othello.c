075cfb51b8643b9381686bedc93dfec8ee157872
May04 <mcechinatto@ucs.br>
2018-11-18 23:59:29 -0200

    mtas coisas feitas, commitando com louvor

--- othello.c
+++ othello.c
@@ -3,15 +3,13 @@
 //Outubro de 2018
 
 //@TODOS:
-//verificar se o movimento é permitido
-//verificar se há jogadas válidas para o determinado jogador
-//fazer "comer" as peças
 //fazer IA
 //contagem de pontos
 //criar armazenagem de highscores
 //fazer contagem da partida
 //Criar menu: Novo Jogo / Ver highscores
 //pedir se deseja reiniciar o jogo
+//fazer fim do jogo
 
 #include <stdio.h>
 #include <string.h>
@@ -26,8 +24,12 @@ char jogada[2];
 int modo;
 //variável que guarda o estado do jogo (em execução = 0 / finalizado = 1)
 int fimDeJogo = 0;
-//variável que define de quem é o turno (ímpar = Branco / par = Preto)
+//variável que define de quem é o turno (ímpar = Preto / par = Branco)
 int turno = 1;
+//variáveis que recebem o código da cor do jogador do turno e do adversário
+int cor, corAdv;
+//array que recebe a posição das jogadas válidas
+int arrayJogadasValidas[60][2];
 
 //método que seta o tabuleiro na sua forma inicial
 void setaTabuleiro()
@@ -50,19 +52,6 @@ void setaTabuleiro()
                 tabuleiro[k][i] = 32;
             }
         }
-    }
-}
-
-//método que insere um valor no tabuleiro
-void insereValorTabuleiro(int linha, int coluna)
-{
-    if (turno % 2 != 0)
-    {
-        tabuleiro[linha][coluna] = 80;
-    }
-    else
-    {
-        tabuleiro[linha][coluna] = 66;
     }
 }
 
@@ -93,11 +82,258 @@ void estabeleceModoDeJogo()
     modo = verificaModo();
 }
 
-//metódo que verifica se o movimento pedido pelo jogador é válido
-void verificaMovimento(int linha, int coluna){
-
-    insereValorTabuleiro(linha, coluna);
+//método que insere uma jogada, criando o peão na casa pedida e convertendo os do adversário
+void insereJogada(int linha, int coluna)
+{
+    tabuleiro[linha][coluna] = cor;
+    int i = coluna - 1;
+    if (tabuleiro[linha][i] == corAdv)
+    {
+        i--;
+        while (i > -1)
+        {
+            if (tabuleiro[linha][i] == corAdv)
+            {
+                i--;
+            }
+            else if (tabuleiro[linha][i] == cor)
+            {
+                int j;
+                for (j = i; j < coluna; j++)
+                {
+                    tabuleiro[linha][j] = cor;
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    i = coluna + 1;
+    if (tabuleiro[linha][i] == corAdv)
+    {
+        i++;
+        while (i < 8)
+        {
+            if (tabuleiro[linha][i] == corAdv)
+            {
+                i++;
+            }
+            else if (tabuleiro[linha][i] == cor)
+            {
+                int j;
+                for (j = i; j > coluna; j--)
+                {
+                    tabuleiro[linha][j] = cor;
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    i = linha + 1;
+    if (tabuleiro[i][coluna] == corAdv)
+    {
+        i++;
+        while (i < 8)
+        {
+            if (tabuleiro[i][coluna] == corAdv)
+            {
+                i++;
+            }
+            else if (tabuleiro[i][coluna] == cor)
+            {
+                int j;
+                for (j = i; j > linha; j--)
+                {
+                    tabuleiro[j][coluna] = cor;
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    i = linha - 1;
+    if (tabuleiro[i][coluna] == corAdv)
+    {
+        i--;
+        while (i > -1)
+        {
+            if (tabuleiro[i][coluna] == corAdv)
+            {
+                i--;
+            }
+            else if (tabuleiro[i][coluna] == cor)
+            {
+                int j;
+                for (j = i; j < linha; j++)
+                {
+                    tabuleiro[j][coluna] = cor;
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    i = linha - 1;
+    int k = coluna -1;
+    if (tabuleiro[i][k] == corAdv)
+    {
+        i--;
+        k--;
+        while (i > -1 && k > -1)
+        {
+            if (tabuleiro[i][k] == corAdv)
+            {
+                i--;
+                k--;
+            }
+            else if (tabuleiro[i][k] == cor)
+            {
+                int j, l;
+                for (j = i; j < linha; j++)
+                {
+                    for(l = k; l < coluna; l++){
+                        tabuleiro[j][l] = cor;
+                    }
+                    
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    i = linha - 1;
+    k = coluna + 1;
+    if (tabuleiro[i][k] == corAdv)
+    {
+        i--;
+        k++;
+        while (i > -1 && k < 8)
+        {
+            if (tabuleiro[i][k] == corAdv)
+            {
+                i--;
+                k++;
+            }
+            else if (tabuleiro[i][k] == cor)
+            {
+                int j, l;
+                for (j = i; j < linha; j++)
+                {
+                    for(l = k; l > coluna; l--){
+                        tabuleiro[j][l] = cor;
+                    }
+                    
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    i = linha + 1;
+    k = coluna + 1;
+    if (tabuleiro[i][k] == corAdv)
+    {
+        i++;
+        k++;
+        while (i < 8 && k < 8)
+        {
+            if (tabuleiro[i][k] == corAdv)
+            {
+                i++;
+                k++;
+            }
+            else if (tabuleiro[i][k] == cor)
+            {
+                int j, l;
+                for (j = i; j > linha; j--)
+                {
+                    for(l = k; l > coluna; l--){
+                        tabuleiro[j][l] = cor;
+                    }
+                    
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    i = linha + 1;
+    k = coluna - 1;
+    if (tabuleiro[i][k] == corAdv)
+    {
+        i++;
+        k--;
+        while (i < 8 && k > -1)
+        {
+            if (tabuleiro[i][k] == corAdv)
+            {
+                i++;
+                k--;
+            }
+            else if (tabuleiro[i][k] == cor)
+            {
+                int j, l;
+                for (j = i; j > linha; j--)
+                {
+                    for(l = k; l < coluna; l++){
+                        tabuleiro[j][l] = cor;
+                    }
+                    
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
     turno++;
+}
+
+//metódo que verifica se o movimento pedido pelo jogador está presente
+//no arrayJogadasValidas e sendo assim é possível
+void verificaMovimento(int linha, int coluna, int array[60][2])
+{
+    int i;
+    int movimentoValido = 0;
+    for (i = 0; i < 60; i++)
+    {
+        if (array[i][0] == linha && array[i][1] == coluna)
+        {
+            movimentoValido++;
+        }
+    }
+    if (movimentoValido > 0)
+    {
+        insereJogada(linha, coluna);
+    }
+    else
+    {
+        printf("\nMovimento Inválido! Tente outra jogada!\n");
+    }
 }
 
 //método que recebe a jogada do jogador
@@ -108,18 +344,17 @@ void pedeJogada()
     if (turno % 2 != 0)
     {
         printf("Jogador P <%s>: ", nome1);
-        scanf("%s", jogada);
     }
     else
     {
         printf("Jogador B <%s>: ", nome2);
-        scanf("%s", jogada);
     }
+    scanf("%s", jogada);
 }
 
 //metódo que verifica a formatação da jogada e a respectiva casa
 //chama função que verifica se o movimento é válido
-void verificaJogada(char jogada[2])
+void verificaFormatacaoJogada(char jogada[2])
 {
     int i, linha, coluna;
     int cont1 = 0;
@@ -148,20 +383,285 @@ void verificaJogada(char jogada[2])
 
     if (contLinha == 0 || contColuna == 0)
     {
-        printf("Valor inserido inválido!\nExemplo de jogada: D6.");
-        pedeJogada();
-        verificaJogada(jogada);
+        printf("Valor inserido inválido!\nVerifique se o valor digitado é composto de uma letra maiúscula entre A e H e um número entre 1 e 8.\nExemplo de jogada: D6.");
     }
     else
     {
-        verificaMovimento(linha, coluna);        
+        if (tabuleiro[linha][coluna] != 32)
+        {
+            printf("Valor inserido inválido!\nA casa selecionada não está vazia!");
+        }
+        else
+        {
+            verificaMovimento(linha, coluna, arrayJogadasValidas);
+        }
     }
 }
 
-//@TODO: método que verifica se existe jogada válida para aquele jogador
-void verificaJogadaValida(){
-    pedeJogada();
-    verificaJogada(jogada);
+//função que verifica as jogadas válidas par ao jogador da vez
+//armazena essas jogadas no arrayJogadasValidas
+//retorna o numero de jogadas válidas ou
+//se retornar -1 tabuleiro está cheio
+//pega a posição da pedra do jogadorda vez
+//verifica se ela pode jogar em linha, coluna ou diagonais
+int jogadasValidas()
+{
+    int i, k, j;
+    int casasVazias = 0;
+    int contJogadasValidas = 0;
+    for (k = 0; k < 8; k++)
+    {
+        for (i = 0; i < 8; i++)
+        {
+            if (tabuleiro[k][i] == 32)
+            {
+                casasVazias++;
+            }
+            if (casasVazias > 0)
+            {
+                if (tabuleiro[k][i] == cor)
+                {
+                    int j = i + 1;
+                    if (tabuleiro[k][j] == corAdv)
+                    {
+                        j++;
+                        while (j < 8)
+                        {
+                            if (tabuleiro[k][j] == corAdv)
+                            {
+                                j++;
+                            }
+                            else if (tabuleiro[k][j] == 32)
+                            {
+                                arrayJogadasValidas[contJogadasValidas][0] = k;
+                                arrayJogadasValidas[contJogadasValidas][1] = j;
+                                contJogadasValidas++;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    j = i - 1;
+                    if (tabuleiro[k][j] == corAdv)
+                    {
+                        j--;
+                        while (j > -1)
+                        {
+                            if (tabuleiro[k][j] == corAdv)
+                            {
+                                j--;
+                            }
+                            else if (tabuleiro[k][j] == 32)
+                            {
+                                arrayJogadasValidas[contJogadasValidas][0] = k;
+                                arrayJogadasValidas[contJogadasValidas][1] = j;
+                                contJogadasValidas++;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    j = k + 1;
+                    if (tabuleiro[j][i] == corAdv)
+                    {
+                        j++;
+                        while (j < 8)
+                        {
+                            if (tabuleiro[j][i] == corAdv)
+                            {
+                                j++;
+                            }
+                            else if (tabuleiro[j][i] == 32)
+                            {
+                                arrayJogadasValidas[contJogadasValidas][0] = j;
+                                arrayJogadasValidas[contJogadasValidas][1] = i;
+                                contJogadasValidas++;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    j = k - 1;
+                    if (tabuleiro[j][i] == corAdv)
+                    {
+                        j--;
+                        while (j > -1)
+                        {
+                            if (tabuleiro[j][i] == corAdv)
+                            {
+                                j--;
+                            }
+                            else if (tabuleiro[j][i] == 32)
+                            {
+                                arrayJogadasValidas[contJogadasValidas][0] = j;
+                                arrayJogadasValidas[contJogadasValidas][1] = i;
+                                contJogadasValidas++;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    j = i + 1;
+                    int l = k + 1;
+                    if (tabuleiro[l][j] == corAdv)
+                    {
+                        j++;
+                        l++;
+                        while (j < 8 && l < 8)
+                        {
+                            if (tabuleiro[l][j] == corAdv)
+                            {
+                                j++;
+                                l++;
+                            }
+                            else if (tabuleiro[l][j] == 32)
+                            {
+                                arrayJogadasValidas[contJogadasValidas][0] = l;
+                                arrayJogadasValidas[contJogadasValidas][1] = j;
+                                contJogadasValidas++;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    j = i - 1;
+                    l = k - 1;
+                    if (tabuleiro[l][j] == corAdv)
+                    {
+                        j--;
+                        l--;
+                        while (j > -1 && l > -1)
+                        {
+                            if (tabuleiro[l][j] == corAdv)
+                            {
+                                j--;
+                                l--;
+                            }
+                            else if (tabuleiro[l][j] == 32)
+                            {
+                                arrayJogadasValidas[contJogadasValidas][0] = l;
+                                arrayJogadasValidas[contJogadasValidas][1] = j;
+                                contJogadasValidas++;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    j = i + 1;
+                    l = k - 1;
+                    if (tabuleiro[l][j] == corAdv)
+                    {
+                        j++;
+                        l--;
+                        while (j < 8 && l > -1)
+                        {
+                            if (tabuleiro[l][j] == corAdv)
+                            {
+                                j++;
+                                l--;
+                            }
+                            else if (tabuleiro[l][j] == 32)
+                            {
+                                arrayJogadasValidas[contJogadasValidas][0] = l;
+                                arrayJogadasValidas[contJogadasValidas][1] = j;
+                                contJogadasValidas++;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    j = i - 1;
+                    l = k + 1;
+                    if (tabuleiro[l][j] == corAdv)
+                    {
+                        j--;
+                        l++;
+                        while (j > -1 && l < 8)
+                        {
+                            if (tabuleiro[l][j] == corAdv)
+                            {
+                                j--;
+                                l++;
+                            }
+                            else if (tabuleiro[l][j] == 32)
+                            {
+                                arrayJogadasValidas[contJogadasValidas][0] = l;
+                                arrayJogadasValidas[contJogadasValidas][1] = j;
+                                contJogadasValidas++;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                contJogadasValidas = -1;
+            }
+        }
+    }
+    return contJogadasValidas;
+}
+
+//método que verifica se existe jogada válida para aquele jogador apartir do valor retornado pela função jogadasValidas
+void verificaJogadaValida()
+{
+    //Limpa o array com jogadas válidas para que novas sejam inseridas
+    memset(arrayJogadasValidas, 0, sizeof(arrayJogadasValidas));
+    if (jogadasValidas() == -1)
+    {
+        printf("Tabuleiro Cheio");
+        fimDeJogo = 1;
+    }
+    else
+    {
+        if (jogadasValidas() > 0)
+        {
+            pedeJogada();
+            verificaFormatacaoJogada(jogada);
+        }
+        else
+        {
+            if (turno % 2 != 0)
+            {
+                printf("\n\nNão há jogada válida para o jogador %s, passou a vez.", nome1);
+            }
+            else
+            {
+                printf("\n\nNão há jogada válida para o jogador %s, passou a vez.", nome2);
+            }
+            turno++;
+            pedeJogada();
+            verificaFormatacaoJogada(jogada);
+        }
+    }
 }
 
 //método que pede e instancia o nome dos jogadores
@@ -219,7 +719,17 @@ void main()
     //e uma nova jogada é solicitada ao jogador da vez
     while (fimDeJogo != 1)
     {
+        if (turno % 2 != 0)
+        {
+            cor = 80;
+            corAdv = 66;
+        }
+        else
+        {
+            cor = 66;
+            corAdv = 80;
+        }
         mostraTabuleiro();
-        verificaJogadaValida();        
+        verificaJogadaValida();
     }
 }
